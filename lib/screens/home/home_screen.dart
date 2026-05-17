3b63764e6ba41cc28ca3bac258de4d5d45704295
alnoman997 <alnoman997@gmail.com>
2026-05-17 08:44:27 +0600

    ui design

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -12,7 +12,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+          leading: Image.asset('assets/icons/logo.png')
       ),
     );
   }
